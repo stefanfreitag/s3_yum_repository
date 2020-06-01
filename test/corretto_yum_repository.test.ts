d40057329b2a0379efd142cff79d4943774860ff
Stefan Freitag <stefan.freitag@udo.edu>
2020-06-01 12:01:02 +0200

    feat: Added properties interface (#45)
    Added test for bucket policy

--- test/corretto_yum_repository.test.ts
+++ test/corretto_yum_repository.test.ts
@@ -1,11 +1,19 @@
-import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import CorrettoYumRepository = require('../lib/corretto_yum_repository-stack');
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+  haveResource,
+  haveResourceLike,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import YumRepository = require("../lib/corretto_yum_repository-stack");
 
 test("S3 buckets are not public accessible ", () => {
   const app = new cdk.App();
 
-  const stack = new CorrettoYumRepository.CorrettoYumRepositoryStack(app, "MyTestStack");
+  const stack = new YumRepository.YumRepositoryStack(app, "MyTestStack", {
+    whitelist: ["87.123.60.75/32"],
+  });
 
   expectCDK(stack).to(
     haveResource("AWS::S3::Bucket", {
@@ -22,7 +30,9 @@ test("S3 buckets are not public accessible ", () => {
 test("S3 buckets are encrypted ", () => {
   const app = new cdk.App();
 
-  const stack = new CorrettoYumRepository.CorrettoYumRepositoryStack(app, "MyTestStack");
+  const stack = new YumRepository.YumRepositoryStack(app, "MyTestStack", {
+    whitelist: ["87.123.60.75/32"],
+  });
 
   expectCDK(stack).to(
     haveResource("AWS::S3::Bucket", {
@@ -32,6 +42,33 @@ test("S3 buckets are encrypted ", () => {
             ServerSideEncryptionByDefault: {
               SSEAlgorithm: "AES256",
             },
+          },
+        ],
+      },
+    })
+  );
+});
+
+test("S3 bucket policy is setup", () => {
+  const app = new cdk.App();
+
+  const stack = new YumRepository.YumRepositoryStack(app, "MyTestStack", {
+    whitelist: ["87.123.60.75/32"],
+  });
+
+  expectCDK(stack).to(
+    haveResourceLike("AWS::S3::BucketPolicy", {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: "s3:GetObject",
+            Effect: "Allow",
+            Principal: "*"
+          },
+          {
+            Action: ["s3:ListBucket", "s3:GetBucketLocation"],
+            Effect: "Allow",
+            Principal: "*"
           },
         ],
       },
