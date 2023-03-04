e3d8b80428f46dc94926383d36cfaaa81f8588ff
Stefan Freitag <stefan.freitag@udo.edu>
2023-03-04 09:47:08 +0100

    chore: upgraded cdk to v2.67.0 (#198)

--- .projenrc.js
+++ .projenrc.js
@@ -4,9 +4,9 @@ const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Stefan Freitag',
   authorAddress: 'stefan.freitag@udo.edu',
-  cdkVersion: '2.65.0',
+  cdkVersion: '2.67.0',
   stability: Stability.EXPERIMENTAL,
-  defaultReleaseBranch: 'main',
+  defaultReleaseBranch: 'master',
   name: 's3_yum_repository_projen',
   repositoryUrl: 'git://github.com/stefanfreitag/s3_yum_repository.git',
   depsUpgradeOptions: {
